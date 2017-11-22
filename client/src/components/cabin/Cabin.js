bcbf64de3fba2bc271e97bfd1cbdc3b45f5222ab
unknown <luca.canizzo@gmail.com>
2017-11-21 21:39:33 -0500

    moved data to cabin to keep at highest needed component, rendering first item from array before iterating through data.

--- client/src/components/cabin/Cabin.js
+++ client/src/components/cabin/Cabin.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import './Cabin.css';
 import Seat from '../seat/Seat';
+import data from '../../data/data';
 
 export default class Cabin extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            seats: data,
+            firstSeat: data[0]
+        }
+
+        // this.state.seats.map((seat)=>{ 
+        //   console.log(`Row: ${seat.row} \n Seat: ${seat.seat}`);
+        // })
+    }
+
     render() {
         return (
             <div className="cabin-box">
-                <Seat />
+               <Seat info={this.state.firstSeat} />
+
+                {
+                    this.state.seats.forEach((seat)=>{
+                        // console.log(`Row: ${seat.row} \n Seat: ${seat.seat}`);
+                        <Seat info={seat}/>
+                    })
+                }
             </div>
         )
     }
--- client/src/components/seat/Seat.js
+++ client/src/components/seat/Seat.js
@@ -3,14 +3,17 @@ import './Seat.css';
 
 export default class Seat extends React.Component {
     constructor(props) {
+        console.log(`Seat Constructor Hit`);
         super(props);
+        // const seat = this.props.info.seat;
+        console.log(`Seat info in Seat.js: \n ${JSON.stringify(this.props.info)}`);
     }
 
     render() {
         return (
             <div className="seat">
-                <p>Row: </p>
-                <p>Seat: </p>
+                <h1>Seat: {this.props.info.seat} </h1>
+                <p>Row: {this.props.info.row}</p>
             </div>
         )
     }
