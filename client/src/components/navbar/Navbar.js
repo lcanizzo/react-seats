d83cc4425a42ea0f63de5933011833417c45c907
unknown <luca.canizzo@gmail.com>
2017-11-21 20:16:00 -0500

    Linked navbar css file

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './Navbar.css';
 
 export default class Navbar extends React.Component {
     render() {
         return (
-            <div className="navbar navbar-dark bg-dark">
-                Navbar: Class Sorting
+            <div className="navbar sticky-top navbar-light  bg-light ">
+                <a className="navbar-brand">Navbar: Class Sorting</a>
             </div>
         )
     }
