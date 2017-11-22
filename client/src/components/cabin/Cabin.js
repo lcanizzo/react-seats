8887356082a852a55c61cad3e698359251819288
unknown <luca.canizzo@gmail.com>
2017-11-21 20:34:01 -0500

    Created basic seat component. Replaced public folder & index.html.

--- client/src/components/cabin/Cabin.js
+++ client/src/components/cabin/Cabin.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './Cabin.css';
+import Seat from '../seat/Seat';
 
 export default class Cabin extends React.Component {
     render() {
         return (
             <div className="cabin-box">
-            
+
             </div>
         )
     }
---
+++ client/src/components/seat/Seat.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Seat.css';
+import data from '../../data/data';
+
+console.log(`Data in Seat.js:\n ${data}`)
+
+export default class Seat extends React.Component {
+    render() {
+        return (
+            <div className="">
+
+            </div>
+        )
+    }
+}
