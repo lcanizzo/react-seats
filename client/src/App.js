0a078c3e7b3b8132ed0d7d6d9af71f085622ad58
unknown <luca.canizzo@gmail.com>
2017-11-21 20:24:06 -0500

    Created basic cabin element

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/navbar/Navbar';
+import Cabin from './components/cabin/Cabin';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="wrapper">
         <Navbar />
+        <Cabin />
       </div>
     );
   }
---
+++ client/src/components/cabin/Cabin.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Cabin.css';
+
+export default class Cabin extends React.Component {
+    render() {
+        return (
+            <div className="cabin-box">
+            
+            </div>
+        )
+    }
+}
