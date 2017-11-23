208861aec0d0467f9f3637d572eba3f6935beae3
unknown <luca.canizzo@gmail.com>
2017-11-23 14:55:47 -0500

    Basic Seperaton of seats by row in DOM

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
   render() {
     return (
       <div className="wrapper">
-        <Navbar />
+      {/* Not rendering Nav for brevity
+        <Navbar /> */}
         <Cabin />
       </div>
     );
--- client/src/components/seatSort/SeatSort.js
+++ client/src/components/seatSort/SeatSort.js
@@ -6,18 +6,73 @@ export default function SortSeats(props) {
         return a.row - b.row
     })
     const sortedSeats = props.data;
-    console.log(`Data passed as props to SeatSort.js`);
-    console.log(props.data);
 
-        return  (
-            <div className='SortedData'>
-                {
-                    props.data.map((seat)=>
-                        // console.log(`Row: ${seat.row} \n Seat: ${seat.seat}`);
-                        <Seat info={seat}/>
-                    )
-                }
-            </div>
-        )
+    //Testing for Row rendering//
+
+
+    //End testing//
+
+    return  (
+// B U L K    R E N D E R I N G
+        // <div className='SortedData'>
+        //     {
+        //         sortedSeats.map((seat, index, array)=>
+        //             <Seat info={seat}/>
+        //         )
+        //     }
+        // </div>
+
+//  F I R S T   R O W 
+        // <div className='SortedData'>
+        //     {
+        //         sortedSeats.map((seat, index)=> {
+        //             if(seat.row === 1) {
+        //                 return (
+        //                     <div className="row-block">
+        //                         <Seat info={seat}/>  
+        //                         <br/><br/>  
+        //                     </div>    
+        //                 );                    
+        //             } else {
+        //                 return (
+        //                     <p>Not in first row</p>
+        //                 )
+        //             }
+        //         })
+        //     }
+        // </div>
+
+//  B Y   R O W
+        <div className='SortedData'>
+            {
+                sortedSeats.map(function cb(currentSeat, index, array) {
+                    // if(index) {
+                    //     console.log(`Row Hit:`);
+                    //     console.log(array[index-1].row);
+                    // }
+                    if(index === 0) {
+                        return (
+                            <Seat info={currentSeat}/>                              
+                        )
+                    } else {
+                        if (currentSeat.row !== array[index-1].row) {
+                            return (
+                                <div className="row-block">
+                                    <br/>
+                                    <h1> New Row </h1>
+                                    <Seat info={currentSeat}/>  
+                                </div>    
+                            );                    
+                        } else {
+                            return (
+                                <Seat info={currentSeat}/>                                                              
+                            )
+                        }
+                    }
+                })
+            }
+        </div>
+
+    )
 }
 
