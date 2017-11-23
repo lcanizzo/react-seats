1a352330f51a26ba3449917d3c5051f6dcbb42a2
unknown <luca.canizzo@gmail.com>
2017-11-23 16:34:55 -0500

    Seats rendering to DOM in divs per row, sorted by seat.

--- client/src/components/seatSort/SeatSort.js
+++ client/src/components/seatSort/SeatSort.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './SeatSort.css';
 import Seat from '../seat/Seat';
 
 export default function SortSeats(props) {
     const seats = props.data.sort(function(a,b) {
         return a.row - b.row
     })
-    
+
 //Testing for Row sorting//
     // Determine Highest Row
     let highestRow = 1;
@@ -19,7 +20,7 @@ export default function SortSeats(props) {
     let rowArrays = [];
     for (let i=1; i<highestRow+1; i++) {
         let rowArray = seats.filter(function(seat) {
-            return seat.row == i;
+            return seat.row === i;
         })
         rowArrays.push(rowArray);
     }
@@ -36,23 +37,24 @@ export default function SortSeats(props) {
     }
 
     // Log rowArrays to console
-    for (let i=0; i<rowArrays.length; i++) {
-        console.log(`rowArrays:\n ${JSON.stringify(rowArrays[i])}`);        
-    }
+    // for (let i=0; i<rowArrays.length; i++) {
+    //     console.log(`rowArrays:\n ${JSON.stringify(rowArrays[i])}`);        
+    // }
 //End testing//
 
     return  (
-
-// B U L K    R E N D E R I N G
-        // <div className='SortedData'>
-        //     {
-        //         seats.map((seat, index, array)=>
-        //             <Seat info={seat}/>
-        //         )
-        //     }
-        // </div>
-
-        <div>
+        <div className="sortedData">
+            {
+                rowArrays.map((row) => 
+                    <div className="rowBox">
+                        {    
+                            row.map((seat) =>
+                                <Seat info={seat}/>
+                            ) 
+                        }
+                    </div>
+                )
+            }
         </div>
     )
 }
